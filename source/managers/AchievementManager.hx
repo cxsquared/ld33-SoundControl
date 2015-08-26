227baf51ea72d24f2fab7e12c4ee1905911abf31
cxsquared <cxsquared@gmail.com>
2015-08-25 21:44:03 -0400

    Added ingore and started adding achievements

---
+++ source/managers/AchievementManager.hx
@@ -0,0 +1,14 @@
+package managers;
+import flixel.FlxG;
+import flixel.addons.api.FlxGameJolt;
+class AchievementManager {
+
+    private var initialized:Bool = false;
+    private var gameID = 88203;
+
+    public static function init():Void {
+        if (!initialized) {
+            FlxGameJolt.init(88203, )
+        }
+    }
+}
